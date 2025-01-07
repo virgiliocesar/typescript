56fb41dbacae322bc2a56c0761541581e1bf11c1
Virgílio César <virgilio_cesar.dev@outlook.com>
2025-01-07 13:22:03 -0300

    config

---
+++ fundamentos_sintaxe/app.js
@@ -0,0 +1,7 @@
+var user = {
+    name: 'John',
+    age: 30,
+    isAdmin: false,
+    courses: ['html', 'css', 'js']
+};
+console.log(user.name); // Johnnpm i
---
+++ fundamentos_sintaxe/app.ts
@@ -0,0 +1,8 @@
+const user = {
+    name: 'John',
+    age: 30,
+    isAdmin: false,
+    courses: ['html', 'css', 'js']
+};
+
+console.log(user.name); // Johnnpm i
